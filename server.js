0f0398f30e7566a72482cbc121fa6fc8a3cd7f6a
Sejal-g3 <sejalgheenah@gmail.com>
2024-11-25 12:31:32 +0400

    Added comments

--- server.js
+++ server.js
@@ -5,16 +5,20 @@ var fs = require("fs");
 
 var app = express();
 
-app.use(morgan("short"));
+// Middleware 1: Log all incoming requests and their URLs
+app.use(morgan("short"));;
 
+// Middleware 2: Serve static files based on request URL
 var staticPath = path.join(__dirname, "../Vue.js-App/images");
 app.use(express.static(staticPath));
 
+// Middleware 3: Handle 404 errors for unspecified files
 app.use(function(req, res) {
     res.status(404);
     res.send("File not found!");
-});   
+});  
 
+// Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
